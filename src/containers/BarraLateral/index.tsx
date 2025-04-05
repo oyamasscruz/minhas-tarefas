50f393a37a43d1714905ee80bdfb94263be10bf3
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-04-05 09:10:14 -0300

    recurso pesquisa / adding search resource

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,20 +1,32 @@
+import { useDispatch, useSelector } from 'react-redux'
 import FiltroCard from '../../components/FiltroCard'
 import * as S from './styles'
+import { RootReducer } from '../../store'
+import { alteraTermo } from '../../store/reducers/filtro'
 
-const BarraLateral = () => (
-  <S.Aside>
-    <div>
-      <S.Campo type="text" placeholder="procurar" />
-      <S.GridFiltro>
-        <FiltroCard legenda="pendentes" contador={1} />
-        <FiltroCard legenda="concluídas" contador={3} />
-        <FiltroCard legenda="urgentes" contador={2} />
-        <FiltroCard legenda="importantes" contador={2} />
-        <FiltroCard legenda="normal" contador={4} />
-        <FiltroCard ativo legenda="todas" contador={12} />
-      </S.GridFiltro>
-    </div>
-  </S.Aside>
-)
+const BarraLateral = () => {
+  const dispatch = useDispatch()
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+  return (
+    <S.Aside>
+      <div>
+        <S.Campo
+          type="text"
+          placeholder="procurar"
+          value={termo}
+          onChange={(evento) => dispatch(alteraTermo(evento.target.value))}
+        />
+        <S.GridFiltro>
+          <FiltroCard legenda="pendentes" contador={1} />
+          <FiltroCard legenda="concluídas" contador={3} />
+          <FiltroCard legenda="urgentes" contador={2} />
+          <FiltroCard legenda="importantes" contador={2} />
+          <FiltroCard legenda="normal" contador={4} />
+          <FiltroCard ativo legenda="todas" contador={12} />
+        </S.GridFiltro>
+      </div>
+    </S.Aside>
+  )
+}
 
 export default BarraLateral
